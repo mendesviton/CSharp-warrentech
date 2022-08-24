b6f9db891ddfe4fc7a9ece9ade8023b461fb2709
Vitor Daniel Mendes Dos Santos <Vitor.Santos@proway.treina>
2022-08-24 14:59:01 -0300

    [feat] classes in c#

---
+++ primeira_solucao/Warren/Aula 2/WarrenTech/WarrenTech/CRUD/Repository.cs
@@ -0,0 +1,34 @@
+﻿using Dia2.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarrenTech.CRUD
+{
+    internal class Repository
+    {
+        public Repository()
+        {
+           this.peopleList = new List<PessoaFisica>();
+        }
+        public List<PessoaFisica> peopleList;
+        public string Create(PessoaFisica pessoa)
+        {
+            this.peopleList.Add(pessoa);
+            return "Pessoa "+pessoa.firstName + " Salva com sucesso";
+        }
+
+        public List<PessoaFisica> Read()
+        {
+            return peopleList;
+        }
+        public string Update(PessoaFisica pessoa)
+        {
+            return "Pessoa " + pessoa.firstName + " alterada com sucesso";
+        }
+
+        public string Delete(int id) { return "Pessoa id: " + id + " deletada com sucesso"; }
+    }
+}
--- primeira_solucao/Warren/Aula 2/WarrenTech/WarrenTech/Program.cs
+++ primeira_solucao/Warren/Aula 2/WarrenTech/WarrenTech/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WarrenTech.CRUD;
 
 namespace WarrenTech
 {
@@ -12,9 +13,30 @@ namespace WarrenTech
         static void Main(string[] args)
         {
             PessoaFisica ppl = new PessoaFisica(fistname: "Vitor",lastname:"Daniel sdasd ",cpf: "1231232");
+            PessoaFisica ppl1 = new PessoaFisica(fistname: "Vitor Daniel", lastname: "Daniel ", cpf: "1231232");
 
-            
+
             System.Console.WriteLine(ppl.Saudacao());
+
+            Repository crud = new Repository();
+
+            System.Console.WriteLine(crud.Create(ppl));
+            System.Console.WriteLine(crud.Create(ppl1));
+            System.Console.WriteLine(crud.Update(ppl));
+
+            /*for (int i = 0; i < crud.Read().Count; i++)
+            {
+                System.Console.WriteLine(crud.peopleList[i].firstName);   
+            }*/
+            foreach (var item in crud.Read())
+            {
+                System.Console.WriteLine(item.Saudacao());
+                
+            }
+
+
+            System.Console.WriteLine(crud.Delete(1));
+            
         }
     }
 }
